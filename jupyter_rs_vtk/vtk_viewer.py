000b42a8093f396b07d60e096571b509e8597fc2
mkeilman <mkeilman@radiasoft.net>
2020-01-28 11:06:04 -0700

    Eschew aliases

--- jupyter_rs_vtk/vtk_viewer.py
+++ jupyter_rs_vtk/vtk_viewer.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-import ipywidgets as widgets
+import ipywidgets
 import traitlets
 
 from jupyter_rs_vtk import gui_utils
@@ -9,8 +9,8 @@ from pykern.pkcollections import PKDict
 from traitlets import Any, Bool, Dict, Float, Instance, Integer, List, Unicode
 
 
-@widgets.register
-class VTK(widgets.DOMWidget, rs_utils.RSDebugger):
+@ipywidgets.register
+class VTK(ipywidgets.DOMWidget, rs_utils.RSDebugger):
     """VTK content"""
     _view_name = Unicode('VTKView').tag(sync=True)
     _model_name = Unicode('VTKModel').tag(sync=True)
@@ -20,11 +20,11 @@ class VTK(widgets.DOMWidget, rs_utils.RSDebugger):
     _model_module_version = Unicode('^0.0.1').tag(sync=True)
 
     actor_state = Dict(default_value={}).tag(sync=True)
-    bg_color = widgets.Color('#ffffff').tag(sync=True)
+    bg_color = ipywidgets.Color('#ffffff').tag(sync=True)
     cam_state = Dict(default_value={}).tag(sync=True)
     model_data = Dict(default_value={}).tag(sync=True)
     poly_alpha = Float(1.0).tag(sync=True)
-    selected_obj_color = widgets.Color('#ffffff').tag(sync=True)
+    selected_obj_color = ipywidgets.Color('#ffffff').tag(sync=True)
     show_edges = Bool(True).tag(sync=True)
     show_marker = Bool(True).tag(sync=True)
     title = Unicode('').tag(sync=True)
@@ -42,7 +42,7 @@ class VTK(widgets.DOMWidget, rs_utils.RSDebugger):
 
     @traitlets.default('layout')
     def _default_layout(self):
-        return widgets.Layout(
+        return ipywidgets.Layout(
             width='50%',
             min_width='25%',
             margin='auto'
@@ -61,8 +61,8 @@ class VTK(widgets.DOMWidget, rs_utils.RSDebugger):
 
 
 # Note we need to subclass VBox in the javascript as well
-@widgets.register
-class Viewer(widgets.VBox, rs_utils.RSDebugger):
+@ipywidgets.register
+class Viewer(ipywidgets.VBox, rs_utils.RSDebugger):
     """VTK viewer - includes controls to manipulate the objects"""
     _model_name = Unicode('ViewerModel').tag(sync=True)
     _view_name = Unicode('ViewerView').tag(sync=True)
@@ -100,7 +100,7 @@ class Viewer(widgets.VBox, rs_utils.RSDebugger):
 
     @traitlets.default('layout')
     def _default_layout(self):
-        return widgets.Layout(align_self='stretch')
+        return ipywidgets.Layout(align_self='stretch')
 
     def _handle_change(self, change):
         self.rsdbg('{}'.format(change))
@@ -168,14 +168,14 @@ class Viewer(widgets.VBox, rs_utils.RSDebugger):
         # don't initialize VTK with data - save until the view is ready
         self.content = VTK()
 
-        self.reset_btn = widgets.Button(description='Reset Camera',
+        self.reset_btn = ipywidgets.Button(description='Reset Camera',
                                         layout={'width': 'fit-content'})
         self.reset_btn.on_click(self._reset_view)
 
         self.axis_btns = PKDict()
         for axis in Viewer._axes:
             self.axis_btns[axis] = PKDict(
-                button=widgets.Button(
+                button=ipywidgets.Button(
                     layout={'width': 'fit-content'}
                 ),
                 dir=1
@@ -183,10 +183,10 @@ class Viewer(widgets.VBox, rs_utils.RSDebugger):
             self._set_axis_btn_desc(axis)
             self.axis_btns[axis].button.on_click(self._set_axis)
 
-        self.orientation_toggle = widgets.Checkbox(value=False, description='Show marker')
-        self.edge_toggle = widgets.Checkbox(value=True, description='Show edges')
+        self.orientation_toggle = ipywidgets.Checkbox(value=False, description='Show marker')
+        self.edge_toggle = ipywidgets.Checkbox(value=True, description='Show edges')
 
-        axis_btn_grp = widgets.HBox(
+        axis_btn_grp = ipywidgets.HBox(
             [self.axis_btns[a]['button'] for a in self.axis_btns],
             layout={
                 'justify-content': 'flex-end'
@@ -194,33 +194,33 @@ class Viewer(widgets.VBox, rs_utils.RSDebugger):
         )
 
         # default layout has fixed width which takes up too much space
-        self.bg_color_pick = widgets.ColorPicker(
+        self.bg_color_pick = ipywidgets.ColorPicker(
             concise=True,
             layout={'width': 'max-content'},
             value=self.content.bg_color
         )
         # separate label to avoid text truncation
-        bg_color_pick_grp = widgets.HBox(
-            [widgets.Label('Background color'), self.bg_color_pick]
+        bg_color_pick_grp = ipywidgets.HBox(
+            [ipywidgets.Label('Background color'), self.bg_color_pick]
         )
 
-        self.obj_color_pick = widgets.ColorPicker(
+        self.obj_color_pick = ipywidgets.ColorPicker(
             concise=True,
             layout={'width': 'max-content'},
             value=self.content.selected_obj_color
         )
-        self.obj_color_pick_grp = widgets.HBox(
-            [widgets.Label('Object color'), self.obj_color_pick]
+        self.obj_color_pick_grp = ipywidgets.HBox(
+            [ipywidgets.Label('Object color'), self.obj_color_pick]
         )
 
-        self.poly_alpha_slider = widgets.FloatSlider(
+        self.poly_alpha_slider = ipywidgets.FloatSlider(
             min=0.0, max=1.0, step=0.05, value=self.content.poly_alpha
         )
-        self.poly_alpha_grp = widgets.HBox(
-            [widgets.Label('Surface Alpha'), self.poly_alpha_slider]
+        self.poly_alpha_grp = ipywidgets.HBox(
+            [ipywidgets.Label('Surface Alpha'), self.poly_alpha_slider]
         )
 
-        view_props_grp = widgets.HBox(
+        view_props_grp = ipywidgets.HBox(
             [bg_color_pick_grp,
              self.obj_color_pick_grp,
              self.poly_alpha_grp,
@@ -229,37 +229,37 @@ class Viewer(widgets.VBox, rs_utils.RSDebugger):
         )
 
         # links the values of two widgets
-        widgets.jslink(
+        ipywidgets.jslink(
             (self.bg_color_pick, 'value'),
             (self.content, 'bg_color')
         )
 
-        widgets.jslink(
+        ipywidgets.jslink(
             (self.obj_color_pick, 'value'),
             (self.content, 'selected_obj_color')
         )
 
-        widgets.jslink(
+        ipywidgets.jslink(
             (self.edge_toggle, 'value'),
             (self.content, 'show_edges')
         )
 
-        widgets.jslink(
+        ipywidgets.jslink(
             (self.orientation_toggle, 'value'),
             (self.content, 'show_marker')
         )
 
-        widgets.jslink(
+        ipywidgets.jslink(
             (self.poly_alpha_slider, 'value'),
             (self.content, 'poly_alpha')
         )
 
-        view_cam_grp = widgets.HBox(
+        view_cam_grp = ipywidgets.HBox(
             [self.reset_btn, axis_btn_grp, self.orientation_toggle],
             layout={'padding': '3px 0px 3px 0px'}
         )
 
-        controls_grp = widgets.VBox(
+        controls_grp = ipywidgets.VBox(
             [view_cam_grp, view_props_grp],
             layout={'padding': '8px 4px 8px 4px'}
         )
