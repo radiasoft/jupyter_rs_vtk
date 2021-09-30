5365bf86a13741e4d9e8e3aae19b90fec3583103
Evan Carlin <evan@carlin.com>
2021-09-30 16:53:03 +0000

    WIP radiasoft/container-beamsim-jupyter#51: Update to JupyterLab 3.x
    Moving to 3.x JupyterLab changed the way extensions are packaged. They
    moved from using NPM for distribution to using PyPi.
    https://jupyterlab.readthedocs.io/en/stable/extension/extension_migration.html#jupyterlab-2-x-to-3-x
    
    This package was never distributed through NPM, always a local
    install. So, this packaging change has little impact on us. But, there
    are some more subtle changes to things like file structure, removing
    dependencies (ex webpack), and changing how the build steps works (ex
    setup.py).
    
    One major change is the extension no longer works in "classic" Jupyter
    Notebooks. Only JupyterLab. Classic Notebooks are being deprecated
    in favor of JupyterLab.

--- jupyter_rs_vtk/__init__.py
+++ jupyter_rs_vtk/__init__.py
@@ -1,9 +1,9 @@
 from ._version import version_info, __version__
 
-def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'static',
-        'dest': 'jupyter-rs-vtk',
-        'require': 'jupyter-rs-vtk/extension'
-    }]
+def _jupyter_labextension_paths():
+    return [
+        {
+            'src': 'labextension',
+            'dest': 'jupyter-rs-vtk',
+        }
+    ]
